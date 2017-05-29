423621aa0a10deaa3ea66dbb98a15c9398a30dbc
Jonathan Hoyt <hoyt@github.com>
2017-05-29 15:59:23 -0700

    Add simple route for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def test_login
+    render body: "hello #{params[:login]}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/:login", to: "application#test_login"
 end
