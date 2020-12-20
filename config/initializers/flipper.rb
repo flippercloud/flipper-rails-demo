da56d32e08e310221faab9080c1972b72b9cac4e
John Nunemaker <nunemaker@gmail.com>
2020-12-20 12:53:08 -0500

    Minor tweaks to initialization
    Lets cloud just pick up the FLIPPER_CLOUD_TOKEN env var.

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -1,15 +1,13 @@
 require "flipper/cloud"
 require "flipper/adapters/active_record"
+require "flipper/middleware/memoizer"
 
 Flipper.configure do |config|
   config.default do
-    token = ENV.fetch("FLIPPER_TOKEN")
-
-    Flipper::Cloud.new(token) do |cloud|
+    Flipper::Cloud.new do |cloud|
       cloud.local_adapter = Flipper::Adapters::ActiveRecord.new
     end
   end
 end
 
-require "flipper/middleware/memoizer"
 Rails.configuration.middleware.use Flipper::Middleware::Memoizer, preload_all: true
