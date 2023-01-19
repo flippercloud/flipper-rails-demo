ffe45ff1142b0bae43a8c34d0e22df2a01a0ec7e
Brandon Keepers <brandon@opensoul.org>
2023-01-19 07:42:12 -0500

    Show how to setup Flipper in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,17 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+# Override Flipper Cloud configuration for tests
+Flipper.configure do |config|
+  config.default { Flipper.new(Flipper::Adapters::Memory.new) }
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  setup do
+    Flipper.instance = nil # Reset previous flipper instance
+  end
 end
