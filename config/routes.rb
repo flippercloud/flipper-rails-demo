ad0048e14b7ae0437c92e30de6f756f3f124ed2d
Garrett Dimon <email@garrettdimon.com>
2023-04-11 14:04:41 -0600

    create an overly-simple registration process

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'home#show'
+
+  # Simple Registration
+  get 'register', to: "users#new", as: :new_user
+  post 'register', to: "users#create", as: :users
 end
