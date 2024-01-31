eb5641d6092c8b6aaecc43607c5f89e48f3b39fd
Garrett Dimon <email@garrettdimon.com>
2024-01-31 09:08:00 -0700

    fix: dev mode and header spacing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,9 +17,6 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
-  # Don't digest assets in dev
-  config.assets.digest = false
-
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
