ab34a7bb69588c403464d6ca4e4c8b5a09f70be0
Garrett Dimon <email@garrettdimon.com>
2024-02-01 11:11:05 -0700

    feat: extract reusable bits for examples

--- app/helpers/demo_helper.rb
+++ app/helpers/demo_helper.rb
@@ -5,6 +5,20 @@ module DemoHelper
     end
   end
 
+  def flag_status(flag)
+    render 'shared/flag_status', flag: flag do
+      yield
+    end
+  end
+
+  def status_item(label, condition)
+    render 'shared/status_item', label: label, condition: condition
+  end
+
+  def uses(examples)
+    render 'shared/uses', examples: examples
+  end
+
   def toggle(label:, form_target:, form_method: :post, code:, cli:)
     style = case form_method
             when :post then :success
