d7c863b9d525a0ae114fe0858df542e20650f370
Garrett Dimon <email@garrettdimon.com>
2024-02-02 15:11:54 -0700

    fix: links to source on GitHub

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,15 @@
 module ApplicationHelper
   def page_footer(file)
+    file = file.delete_prefix(Rails.root.to_s)
+
+    # Heroku includes "/app" in Rails.root and removes it. This adds it back
+    #   if missing so the locations are accurate.
+    file = "/app" + file unless file.start_with?("/app")
+
     render 'shared/page_footer', locals: { file: file }
   end
 
   def flash
     render 'shared/flash'
   end
-
 end
